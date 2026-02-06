026d87094e4cf0dccf0da7268052d868948beb72
Nemanja <nemanjamijatovic93@gmail.com>
2026-02-06 16:07:56 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from './sections/Header'
+import Hero from './sections/Hero'
+import Features from './sections/Features'
+import Pricing from './sections/Pricing'
+import Footer from './sections/Footer'
+
+function App() {
+  return (
+    <>
+      <Header Saas />
+      <Hero />
+      <Features />
+      <Pricing />
+      <Footer />
+    </>
+  )
+}
+
+export default App
